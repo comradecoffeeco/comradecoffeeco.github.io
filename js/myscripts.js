e30aed68d7c5aff23bbeba351b827272c5407229
comradecoffeeco <robyn@pro-active.co.za>
2023-12-01 22:43:10 +0200

    comrade coffee new site

---
+++ js/myscripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    // Toggle div display
+    $("#hide-show").click(function(){
+      $(".my-sidebar-wrapper").toggleClass("stayFlexy");
+    });
+});
+
+$(document).ready(function(){
+    // Toggle div display
+    $("#home-button").click(function(){
+      $(".my-sidebar-wrapper").toggleClass("stayFlexy");
+    });
+});
+
+$(document).ready(function(){
+    // Toggle div display
+    $("#about-button").click(function(){
+      $(".my-sidebar-wrapper").toggleClass("stayFlexy");
+    });
+});
+
+$(document).ready(function(){
+    // Toggle div display
+    $("#menu-button").click(function(){
+      $(".my-sidebar-wrapper").toggleClass("stayFlexy");
+    });
+});
+
+$(document).ready(function(){
+    // Toggle div display
+    $("#contact-button").click(function(){
+      $(".my-sidebar-wrapper").toggleClass("stayFlexy");
+    });
+});
